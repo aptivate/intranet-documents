160a74397094ba1af8fe999ad4563e1365cf056f
Chris Wilson <chris+github@qwirx.com>
2012-06-11 16:46:48 +0100

    Move DocumentForm into own module.

---
+++ forms.py
@@ -0,0 +1,63 @@
+from django.core.validators import EMPTY_VALUES
+from django.forms import ModelForm
+from django.forms.util import ErrorList
+
+from binder.admin import AdminYesNoWidget, AdminFileWidgetWithSize
+
+from models import Document
+
+class DocumentForm(ModelForm):
+    MULTIPLE_SELECT_HELP = u'Hold down Ctrl to select multiple options'
+    
+    def __init__(self, request, data=None, files=None, auto_id='id_%s', 
+        prefix=None, initial=None, error_class=ErrorList, label_suffix=':', 
+        empty_permitted=False, instance=None):
+        
+        # import pdb; pdb.set_trace()
+        
+        super(DocumentForm, self).__init__(data, files, auto_id, prefix, 
+            initial, error_class, label_suffix, empty_permitted, instance)
+        self.request = request
+        self.fields['title'].required = False
+        self.fields['document_type'].queryset.order_by('name')
+        self.fields['programs'].help_text = self.MULTIPLE_SELECT_HELP
+        self.fields['programs'].queryset.order_by('name')
+        self.fields['authors'].required = False
+        self.fields['authors'].help_text = self.MULTIPLE_SELECT_HELP
+        self.fields['authors'].queryset.order_by('full_name')
+        self.fields['confidential'].widget = AdminYesNoWidget()
+        self.fields['uploader'].required = False
+        self.fields['file'].widget = AdminFileWidgetWithSize()
+        
+    class Meta:
+        model = Document
+        fields = ('title', 'document_type', 'programs', 'notes',
+            'confidential', 'file', 'hyperlink', 'authors',
+            'external_authors', 'uploader')
+
+    def clean(self):
+        """
+        If the title is not set, default to the name of the attached
+        file with any extension (without spaces) removed.
+        
+        If the authors are not set, set the author to the current logged-in
+        user.
+        """
+        
+        cleaned_data = ModelForm.clean(self)
+        
+        if (cleaned_data['title'] in EMPTY_VALUES and
+            cleaned_data.get('file', None) is not None):
+            import re
+            m = re.match('(.+)\.(\w+)', cleaned_data['file'].name)
+            
+            if m is not None:
+                cleaned_data['title'] = m.group(1)
+            else:
+                cleaned_data['title'] = cleaned_data['file'].name
+
+        if (cleaned_data['authors'] in EMPTY_VALUES):
+            cleaned_data['authors'] = [self.request.user]
+                
+        return cleaned_data
+
